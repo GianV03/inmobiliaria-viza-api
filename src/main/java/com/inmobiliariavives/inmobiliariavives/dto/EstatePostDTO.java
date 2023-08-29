859a3171f978cc77caa020ea8ffa8a7270d0afb8
Giangranco Vives <gianfranco.vives.03@gmail.com>
2023-08-29 00:12:47 -0500

    Se creo Post DTO de Estate

--- src/main/java/com/inmobiliariavives/inmobiliariavives/dto/EstatePostDTO.java
+++ src/main/java/com/inmobiliariavives/inmobiliariavives/dto/EstatePostDTO.java
@@ -1,2 +1,28 @@
-package com.inmobiliariavives.inmobiliariavives.dto;public class EstatePostDTO {
+package com.inmobiliariavives.inmobiliariavives.dto;
+
+import com.inmobiliariavives.inmobiliariavives.models.UserEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class EstatePostDTO {
+    private String title;
+    private String description;
+    private String location;
+    private Double price;
+    private Integer bedrooms;
+    private Integer bathrooms;
+    private UUID userId;
+    private Integer area;
+    private UUID modalityId;
+    private String imageSource;
+    private String images;
+    private String department;
+    private String province;
+    private String district;
+    private UUID creationUser;
+
 }
