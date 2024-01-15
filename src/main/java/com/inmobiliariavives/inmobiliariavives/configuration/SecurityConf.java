2520679a68f823cba60d7e75f5dc874a1701d853
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-01-14 23:01:50 -0500

    se quito configuración de seguridad

--- src/main/java/com/inmobiliariavives/inmobiliariavives/configuration/SecurityConf.java
+++
@@ -1,9 +0,0 @@
-package com.inmobiliariavives.inmobiliariavives.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SecurityConf {
-
-}
