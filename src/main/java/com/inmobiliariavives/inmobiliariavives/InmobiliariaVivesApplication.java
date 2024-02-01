749e0f9baf40b396845fe0172518133c529c6cd2
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-01-31 22:15:00 -0500

    Se quitó el patch en allowedmethods

--- src/main/java/com/inmobiliariavives/inmobiliariavives/InmobiliariaVivesApplication.java
+++ src/main/java/com/inmobiliariavives/inmobiliariavives/InmobiliariaVivesApplication.java
@@ -36,7 +36,7 @@ public class InmobiliariaVivesApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("*")
-						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*");;
 			}
 		};
