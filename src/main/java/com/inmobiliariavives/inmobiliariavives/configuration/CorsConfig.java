3db035b5ef0128a51373354ebd7f264ce40fb9b4
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-01-31 21:28:48 -0500

    Se añadió la configuración de cors en un nuevo archivo 1

--- src/main/java/com/inmobiliariavives/inmobiliariavives/configuration/CorsConfig.java
+++ src/main/java/com/inmobiliariavives/inmobiliariavives/configuration/CorsConfig.java
@@ -1,19 +1,27 @@
 package com.inmobiliariavives.inmobiliariavives.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://www.vizainmobiliaria.com", "https://vizainmobiliaria.com")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedHeaders("*");
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://vizainmobiliaria.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*")); // Puedes personalizar los encabezados permitidos según tus necesidades
+        configuration.setAllowCredentials(true); // Permite incluir credenciales en las solicitudes (por ejemplo, cookies)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
