399e42f6a9cc6d845fdee6f8e07939a7b28ee869
Giangranco Vives <gianfranco.vives.03@gmail.com>
2024-01-31 21:47:53 -0500

    Cambio en allowedheaders

--- src/main/java/com/inmobiliariavives/inmobiliariavives/configuration/CorsConfig.java
+++ src/main/java/com/inmobiliariavives/inmobiliariavives/configuration/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("https://vizainmobiliaria.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("*")); // Puedes personalizar los encabezados permitidos según tus necesidades
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization")); // Puedes personalizar los encabezados permitidos según tus necesidades
         configuration.setAllowCredentials(true); // Permite incluir credenciales en las solicitudes (por ejemplo, cookies)
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
